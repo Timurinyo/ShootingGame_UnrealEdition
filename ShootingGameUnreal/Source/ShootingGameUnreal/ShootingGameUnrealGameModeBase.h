da26ed899fcad1d600f7dc5bea2b03270a3357a3
Dmytro Shalimov <Dmytro.Shalimov@ubisoft.com>
2019-12-17 18:33:15 +0200

    Initial commit

---
+++ ShootingGameUnreal/Source/ShootingGameUnreal/ShootingGameUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShootingGameUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTINGGAMEUNREAL_API AShootingGameUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
